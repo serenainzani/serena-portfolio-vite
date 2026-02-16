853c20a684214f5995eeaa5180527af4222ee918
Serena Inzani <serenainzani@hotmail.com>
2026-02-16 22:57:18 +0000

    feat: desktop design

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,11 @@ import ProjectCard from "./components/ProjectCard";
 function App() {
     return (
         <>
-            <nav className="flex justify-between items-center w-full h-14 bg-fishing-aqua fixed">
+            <nav className="flex justify-between items-center w-full h-14 md:h-16 bg-fishing-aqua fixed">
                 <a href="#top">
                     <img
                         src={logo}
-                        className="logo w-12 m-3"
+                        className="logo w-12 md:w-14 m-3"
                         alt="Serena logo"
                     />
                 </a>
@@ -20,24 +20,28 @@ function App() {
                 >
                     <img
                         src={githubIcon}
-                        className="logo w-12 m-3"
-                        alt="menu icon"
+                        className="logo w-12 md:w-14 m-3"
+                        alt="github"
                     />
                 </a>
             </nav>
-            <hgroup className="font-roboto-slab pt-14">
+            <hgroup className="font-roboto-slab pt-14  md:pt-18">
                 <div className="font-semibold text-fishing-peach justify-self-center text-shadow-[3px_3px_0_#197978]">
-                    <h1 className="text-6xl tracking-tighter pl-11">Serena</h1>
-                    <h1 className="text-9xl tracking-tighter -mt-11">Inzani</h1>
+                    <h1 className="text-6xl md:text-8xl tracking-tighter pl-11 md:pl-16">
+                        Serena
+                    </h1>
+                    <h1 className="text-9xl md:text-[12rem]  tracking-tighter -mt-11 md:-mt-18">
+                        Inzani
+                    </h1>
                 </div>
-                <div className="bg-fishing-aqua w-10/12 justify-self-center h-10 content-center">
-                    <p className="tracking-tighter text-2xl font-semibold font-roboto-slab justify-self-center text-shadow-[2px_1px_0_#000] text-fishing-yellow">
+                <div className="bg-fishing-aqua w-10/12 md:w-3/12 justify-self-center h-10 md:h-14 content-center">
+                    <p className="tracking-tighter text-2xl md:text-4xl font-semibold font-roboto-slab justify-self-center text-shadow-[2px_1px_0_#000] text-fishing-yellow">
                         Software Engineer
                     </p>
                 </div>
             </hgroup>
-            <main className="border-fishing-yellow border-3 m-3 bg-white/50">
-                <h2 className=" justify-self-center font-roboto-slab font-semibold text-2xl tracking-tighter text-fishing-aqua text-shadow-[1px_1px_0_#f2e070] py-2">
+            <main className="border-fishing-yellow border-3 m-3 md:px-10 md:py-3 bg-white/50 md:w-5/12 justify-self-center">
+                <h2 className=" justify-self-center font-roboto-slab font-semibold text-2xl md:text-4xl tracking-tighter text-fishing-aqua text-shadow-[1px_1px_0_#f2e070] py-2">
                     My Projects
                 </h2>
                 <ProjectCard project="rpgNotes" />
--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -4,18 +4,20 @@ import ProjectLink from "./ProjectLink";
 const ProjectCard = ({ project }: { project: string }) => {
     const { title, description, githubLink, liveLink, img } = PROJECTS[project];
     return (
-        <section className="flex w-fit">
+        <section className="flex w-fit md:mb-5">
             <img
                 src={`../../public/${img}`}
                 className="w-40 h-40 object-cover m-2
                 "
             />
             <div className="flex flex-col justify-between">
-                <p className="text-xs font-roboto font-normal p-2">
-                    <span className="text-base font-bold">{title}</span>
+                <p className="text-xs md:text-base font-roboto font-normal p-2">
+                    <span className="text-base md:text-lg font-bold">
+                        {title}
+                    </span>
                     &nbsp;{description}
                 </p>
-                <p className="text-sm font-roboto font-normal p-2">
+                <p className="text-sm md:text-base font-roboto font-normal p-2">
                     {liveLink && (
                         <>
                             <ProjectLink url={liveLink} text="Live App" />
